916f582527b02136ccd5b9b065bd7e5e742c5f78
TN Botz <163137175+sakthiraj2008@users.noreply.github.com>
2025-03-16 02:03:23 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "11472991"))
+API_HASH = environ.get("API_HASH", "c78c50d54baf2173e8b3f75c359c0c72")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7682114390:AAFPo30YL9Z-TsQphIYvWv9pVz-gHHtmnfA")
 
 # Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002318167392"))
+ADMINS = int(environ.get("ADMINS", "-1002318167392"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "-1002318167392") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
 
 # If this is True Then Bot Accept New Join Request 
-NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
+NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
